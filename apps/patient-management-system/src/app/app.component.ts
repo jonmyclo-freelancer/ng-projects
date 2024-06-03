48220a1c4054d08163cbc3932f8e84a3f1fa80d9
Jon Myclo Faustino <jonmyclofaustino@gmail.com>
2024-06-03 22:57:59 +0800

    Add code auth

--- apps/patient-management-system/src/app/app.component.ts
+++ apps/patient-management-system/src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { PrescriptionComponent } from '@ng-projects/prescription';
+import { InputTextModule } from 'primeng/inputtext';
+
+const CODE = '0117173';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, PrescriptionComponent],
+  imports: [RouterModule, PrescriptionComponent, FormsModule, InputTextModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  codeInput = '';
+
+  get isLogin(): boolean {
+    return this.codeInput === CODE;
+  }
+}
